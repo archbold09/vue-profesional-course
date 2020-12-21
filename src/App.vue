315f9da817b77d596e8a0cc73ef506f23c6053da
angel_archbold <angelarchbold09@gmail.com>
2020-12-20 21:12:14 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  #app
+    h1 {{ msg }}
+    p {{ 1 + 1 }}
+    p {{ 'Hola' + 'Mundo' }}
+    p {{ person.name }}
+    p {{ person.name.toUpperCase() }}
+    p {{ JSON.stringify(person) }}
+    p {{ true ? 'true' : 'false' }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola Vue!',
+
+      person: {
+        name: 'Juan'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss'
+</style>
